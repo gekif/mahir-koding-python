82e026e1c7ccd3e330277885b850bfce65ea4ee4
Fikar4F <dzulfikar.maulana@gmail.com>
2025-12-26 15:31:23 +0700

    Mahir Koding Python - Variable and Data Types

---
+++ variables.py
@@ -0,0 +1,14 @@
+angka = 5
+angka_berkoma = 3.85
+nama = "J2W"
+umur = 20
+
+print(f"Angka: {angka}")
+print(f"Angka Berkoma: {angka_berkoma}")
+print(f"Nama: {nama}")
+print(f"Nama saya adalah {nama} dan umur saya {umur} tahun")
+
+print("******")
+
+nama = input("Nama lengkap: ")
+print(f"Selamat datang {nama.title()}!!!")
