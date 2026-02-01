1382dc80962d3764d452c71d4b92597bb9af05f9
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-01 08:32:59 +0700

    Mahir Koding Python - String

---
+++ string.py
@@ -0,0 +1,32 @@
+# Usual
+print("Selamat datang di Coding Studio!")
+
+# Escape Character
+print("Selamat datang di \"Coding Studio!\"")
+
+# String Methods
+text = "Belajar Python"
+print(text.upper())
+print(text.split(' '))
+print(text.index('a'))
+
+# String Slicing
+print(text[0])
+print(text[2])
+print(text[2:])
+print(text[2:5])
+print(text[0:5])
+print(text[:5])
+
+# String Concatenation
+mangga = 5
+apel = 7
+pisang = 4
+text = f"Cecep membeli {mangga} mangga, {apel} apel, dan {pisang} pisang"
+print(text)
+
+# In VS Not In
+text_2 = "Saya senang belajar Python."
+print("Python" in text_2)
+print("Javascript" in text_2)
+
