e101013f90087c1b608089074cb364e1f0fc59b7
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-07 10:55:08 +0700

    Mahir Koding Python - Conditional Part 2

--- conditional.py
+++ conditional.py
@@ -9,3 +9,31 @@ print(not(x > y))
 
 print(x < y and y > z)
 print(x < y and y > z)
+
+if x == y:
+    print("X sama dengan Y")
+
+if x < y:
+    print("X kurang dari Y")
+
+
+if x > y:
+    print("X lebih besar dari Y")
+else:
+    print("X kurang dari Y")
+
+
+if x > y and z < y:
+    print("X lebih besar dari Y")
+else:
+    print("X kurang dari Y")
+
+
+if x > y:
+    print("X lebih besar dari Y")
+elif x < y:
+    print("X kurang dari Y")
+elif z > y:
+    print("Z lebih besar dari Y")
+else:
+    print("Kondisi diatas semuanya salah")
