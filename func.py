ccfdb47a95b0e8e7b349629370280d03294b79f0
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-11 15:41:54 +0700

    Mahir Koding Python - Function Part 1

---
+++ func.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello stranger")
+    print("Nice to meet you")
+
+greet()
+greet()
+
+print("\n")
+
+def greet_with_argument(name, age):
+    print(f"Hello {name}")
+    print(f"You are {age} years old")
+
+greet_with_argument(name="Fikar", age=38)
