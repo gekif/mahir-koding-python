388e7276479517a615075fe0fc2ac902ae27a9b3
Fikar4F <dzulfikar.maulana@gmail.com>
2026-02-11 15:46:02 +0700

    Mahir Koding Python - Function Part 2

--- func.py
+++ func.py
@@ -12,3 +12,21 @@ def greet_with_argument(name, age):
     print(f"You are {age} years old")
 
 greet_with_argument(name="Fikar", age=38)
+
+print("\n")
+
+def add5(number):
+    total = number + 5
+    return total
+
+print(add5(10))
+
+print("\n")
+number = 10
+number_added_5 = add5(number)
+print(number_added_5)
+
+print("\n")
+number = 20
+total = lambda num: num + 5
+print(total(number))
